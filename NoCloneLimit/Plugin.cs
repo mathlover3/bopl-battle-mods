ad4fad2d66cdf51698088ce854d4c3c19a922b51
David H <djhlovesmath@gmail.com>
2024-03-15 13:44:29 -0700

    NoCloneLimit

---
+++ NoCloneLimit/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BoplFixedMath;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using Steamworks;
+using Steamworks.Data;
+
+namespace NoCloneLimit
+{
+    [BepInPlugin("com.David_Loves_JellyCar_Worlds.NoCloneLimit", "NoCloneLimit", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("Plugin NoCloneLimit is loaded!");
+
+            Harmony harmony = new Harmony("com.David_Loves_JellyCar_Worlds.NoCloneLimit");
+
+            Logger.LogInfo("harmany created");
+            harmony.PatchAll();
+            Logger.LogInfo("NoCloneLimit Patch Compleate!");
+        }
+
+
+    [HarmonyPatch(typeof(PlayerCollision))]
+    public class myPatches
+    {
+        [HarmonyPatch("SpawnClone")]
+        [HarmonyPrefix]
+        private static void OnChatMessageCallback_NoCloneLimit_Plug(ref int ___maxAllowedClonesAndBodies)
+        {
+            Debug.Log("cloneing");
+            ___maxAllowedClonesAndBodies = 1000000;
+        }
+    }
+
+        }
+    }
